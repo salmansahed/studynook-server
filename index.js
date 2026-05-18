58d6993239e99e41d6157b0d849e9d6f41d46ea3
salmansahed <salmansahed10@gmail.com>
2026-05-18 20:49:17 +0600

    implement GET API for available study rooms with sorting and limit

---
+++ index.js
@@ -0,0 +1,49 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+const uri = process.env.MONGODB_URI;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+async function run() {
+  try {
+    await client.connect();
+    const db = client.db("studynook-auth");
+    const roomsCollection = db.collection("roomsCollection");
+
+    //For - Available Study Rooms
+    app.get("/available-study-rooms", async (req, res) => {
+      const cursor = roomsCollection.find().sort({ _id: -1 }).limit(6).toArray();
+      const result = await cursor;
+      res.send(result);
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
